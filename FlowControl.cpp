da6736b3b9007bb7ddf8c1e36f915ce99a6cb304
OM_PRADHAN25 <pradhanom149@gmail.com>
2025-10-02 14:27:17 +0530

    Flow Control
    if
    if-else
    if-else if
    if-else if-else
    Nested if

---
+++ FlowControl.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // int a = 5;
+    // if(a==5){
+    //     cout<<"5"<<endl;
+    // }
+    // int age;
+    // cout<<"Enter the age"<<endl;
+    // cin>>age;
+    // if(age>18){
+    //     cout<<"Your are an adult"<<endl;
+    // }
+    // else{
+    //     cout<<"Your are a minor";
+    // }
+    // int age;
+    // cout<<"Enter the age"<<endl;
+    // cin>>age;
+    // if(age>15){
+    //     cout<<"You are a 15 year old baby";
+    // }
+    // else if(age>10){
+    //     cout<<"You are a 10 year old baby";
+    // }
+    // else if(age>5){
+    //     cout<<"You are a 5 year old baby";
+    // }
+    // else if(age>0){
+    //     cout<<"You are a new born baby";
+    // }
+    // else{
+    //     cout<<"You are an adult";
+    // }
+    int h,w;
+    cout<<"enter the height"<<endl;
+    cin>>h;
+    cout<<"enter the weight"<<endl;
+    cin>>w;
+    if(h>5){
+        if(w<80){
+            cout<<"you are fit";
+        }
+        else{
+            cout<<"You are unfit";
+        }
+    }
+    else{
+        cout<<"You are beyond";
+    return 0;
+}
